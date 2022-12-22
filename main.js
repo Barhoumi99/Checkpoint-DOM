1209dbddf4ff153c45d83a43f96827dfb9f2cb8c
barhoumi <wael.barhoumi14@gmail.com>
2022-12-22 17:51:59 +0100

    Added the 'favourite' feature, clicking the heart icon will toggle its color now

--- main.js
+++ main.js
@@ -1,2 +1,38 @@
-var pickedItems = document.getElementsByClassName(pickedItems);
-console.log(pickedItems);
+const shop = document.querySelector(".shop");
+const articles = document.querySelectorAll(".article");
+const addButtons = document.querySelectorAll(".add-button");
+const pickedItems = document.querySelector('.picked-items');
+const hearts = document.querySelectorAll('.bi-heart-fill');
+
+//-----------------------------------
+
+const div = document.createElement('div');
+//-----------------------------------
+
+function createItem(_class, name) {
+    let item = document.createElement('div');
+    item.innerHTML = `<div class=${_class} name=${name}>item</div>`;
+    return item;
+};
+
+// function toggleColor(color) {
+//     if (color == '#505050') {
+//         color = '#ff3232';
+//     } else {
+//         color = '#505050';
+//     }
+// }
+//-----------------------------------
+
+for (let i = 0; i < hearts.length; i++) {
+    hearts[i].style.color = "rgb(80, 80, 80)"; //gray
+    hearts[i].addEventListener('click', () => {
+
+        if (hearts[i].style.color === "rgb(255, 50, 50)") { //red
+            console.log(hearts[i].style.color)
+            hearts[i].style.color = "rgb(80, 80, 80)"; //gray
+        } else { //gray
+            hearts[i].style.color = "rgb(255, 50, 50)"; //red
+        }
+    })
+}
